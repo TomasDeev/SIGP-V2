ac5b2719ff7c8d78d2d6b8fbe7ec3de156bbfab2
TomasDeev <jdiaz@clavoservices.com>
2025-09-25 10:31:35 -0400

    Error de vite con las rutas que tenemos.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
+    historyApiFallback: true
   },
   plugins: [react()],
   resolve: {
